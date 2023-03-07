4c44b8187190cd91035a3711f8f69052f7009e78
Llllluojp <395238895@qq.com>
2023-03-07 15:27:44 +0800

    测试代码

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import legacy from '@vitejs/plugin-legacy'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    legacy({
+      targets: ['chrome 63'],
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
+      renderLegacyChunks: true,
+    })
+  ],
+})
